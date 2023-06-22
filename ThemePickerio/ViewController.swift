4e21d6902ab8e01a0ebc46e0b0a9cb19a0663974
Allicia Viona <alliciawn@gmail.com>
2023-06-23 06:33:57 +0700

    - Veggies and fruits button added - ThemePickerDelegate added - Navigation between ViewController and ThemePickerViewController added

--- ThemePickerio/ViewController.swift
+++ ThemePickerio/ViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+protocol ThemePickerDelegate {
+    func getThemeName(name: String)
+    func getThemeColor(color: UIColor)
+}
 
+class ViewController: UIViewController, ThemePickerDelegate {
+    
+    var themeName: String = "🥦"
+    var themeColor: UIColor = .systemGreen
+
+    @IBOutlet weak var lblEmoji: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        lblEmoji.text = themeName
+        view.backgroundColor = themeColor
     }
 
-
+    @IBAction func pressChangeTheme(_ sender: Any) {
+        let themePickerStoryboard = UIStoryboard(name: "ThemePicker", bundle: nil)
+        let themePickerVC = themePickerStoryboard.instantiateViewController(withIdentifier: "ThemePickerViewController") as! ThemePickerViewController
+        themePickerVC.themePickerDelegate = self
+        themePickerVC.backgroundColor = themeColor
+        present(themePickerVC, animated: true, completion: nil)
+    }
+    
+    func getThemeName(name: String) {
+        themeName = name
+        lblEmoji.text = themeName
+    }
+    
+    func getThemeColor(color: UIColor) {
+        themeColor = color
+        view.backgroundColor = themeColor
+    }
+    
 }
 
